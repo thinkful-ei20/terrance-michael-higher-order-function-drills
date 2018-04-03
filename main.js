6fceb3f0c7dc86c71b6214fed70883fc07f82ce8
Terrance Corley <terrancecorley@outlook.com>
2018-04-03 11:53:50 -0700

    Added functions as argument drill 1-1.

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-function repeat(fn, number){
+'use strict';
 
+
+function repeat(fn, n){
+	for (let i = 1; i <= n; i++) {
+		fn();
+	}
 }
 
 function hello(){
-    console.log('Hello world');
+  console.log('Hello world');
 }
 
 function goodbye(){
-    console.log('Goodbye world');
+ console.log('Goodbye world');
 }
+
+repeat(hello, 5);
+repeat(goodbye, 5);
