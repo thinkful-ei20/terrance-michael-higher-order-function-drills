24fe2ff8b61e073f7cd1009122bb0a1a0791aa44
Terrance Corley <terrancecorley@outlook.com>
2018-04-03 12:29:42 -0700

    Added functions as arguments 1-2 drill

--- main.js
+++ main.js
@@ -1,19 +1,48 @@
 'use strict';
 
 
-function repeat(fn, n){
-	for (let i = 1; i <= n; i++) {
-		fn();
+// function repeat(fn, n){
+// 	for (let i = 1; i <= n; i++) {
+// 		fn();
+// 	}
+// }
+
+// function hello(){
+//   console.log('Hello world');
+// }
+
+// function goodbye(){
+//  console.log('Goodbye world');
+// }
+
+// repeat(hello, 5);
+// repeat(goodbye, 5);
+
+// DO NOT EDIT BETWEEN THESE LINES ----->
+// Return only names that begin with 'R'
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+
+
+
+ // => ['Rich', 'Ray']
+// <---- DO NOT EDIT BETWEEN THESE LINES
+
+// TASK: DEFINE YOUR FILTER FUNCTION BELOW:
+function filter(arr, fn) {
+	const newArray = [];
+
+	for (let item of arr) {
+		if (fn(item) === true) {
+			newArray.push(item);
+		}
 	}
+
+	return newArray;
 }
 
-function hello(){
-  console.log('Hello world');
-}
+const filteredNames = filter(myNames, function(name) {
+	// This is a "predicate function" - it's a function that only returns a boolean
+	return name[0] === 'R';
+});
 
-function goodbye(){
- console.log('Goodbye world');
-}
-
-repeat(hello, 5);
-repeat(goodbye, 5);
+console.log(filteredNames);
